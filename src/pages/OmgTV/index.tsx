69d7db1d1d323bfa36c5c3bb65bcaba0cd151a19
luoyisen <1402175410.@qq.com>
2024-04-15 17:54:41 +0800

    fix

--- src/pages/OmgTV/index.tsx
+++ src/pages/OmgTV/index.tsx
@@ -1,16 +1,24 @@
 import React, { useEffect } from 'react'
+import { Toast } from 'react-vant'
 import { io, Socket } from 'socket.io-client'
 
 const OmgTV: React.FC = () => {
     useEffect(() => {
-        const socket: Socket = io('http://localhost:15002')
+        const urls = {
+            test: 'http://localhost:15002',
+            development: 'http://localhost:15002',
+            production: 'https://luoyisen.com/server'
+        }
+        const socket: Socket = io(urls[process.env.NODE_ENV])
 
         const onconnectHandler = () => {
             console.log(socket.id) // x8WIv7-mJelg7on_ALbx
+            Toast('连接服务器成功')
         }
 
         const ondisconnectHandler = () => {
             console.log(socket.id) // undefined
+            Toast('连接断开')
         }
 
         socket.on('connect', onconnectHandler)
