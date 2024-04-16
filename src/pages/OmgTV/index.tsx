3efb4b9980a8050631f2753cdcea75f98f0afffa
luoyisen <1402175410.@qq.com>
2024-04-16 17:27:43 +0800

    OmgTV

--- src/pages/OmgTV/index.tsx
+++ src/pages/OmgTV/index.tsx
@@ -1,37 +1,150 @@
-import React, { useEffect } from 'react'
-import { Toast } from 'react-vant'
+import React, { useState, useEffect, useRef } from 'react'
+import { Toast, Button } from 'react-vant'
 import { io, Socket } from 'socket.io-client'
+import styles from './index.module.scss'
 
 const OmgTV: React.FC = () => {
+    /**
+     * 0：初始状态
+     * 1：匹配中
+     * 2：匹配成功，连线中
+     * 3：连线成功
+     */
+    const [status, setStatus] = useState<number>(0)
+    const [userCount, setUserCount] = useState<number>(0)
+    const socket = useRef<Socket | null>(null)
+    const roomId = useRef<string>('')
+    const peer = useRef<RTCPeerConnection | null>(null)
+    const stream = useRef<MediaStream | null>(null)
+
+    const themVideo = useRef<HTMLVideoElement | null>(null)
+    const meVideo = useRef<HTMLVideoElement | null>(null)
+
+    const startMatch = () => {
+        socket.current?.emit('match')
+    }
+
+    const leaveRoom = () => {
+        socket.current?.emit('leaveRoom')
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const startSignaling = async (remoteOffer: any) => {
+        peer.current = new RTCPeerConnection()
+        stream.current = await navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+        if (meVideo.current) meVideo.current.srcObject = stream.current
+        stream.current.getTracks().forEach((track) => {
+            peer.current?.addTrack(track, stream.current as MediaStream)
+        })
+
+        peer.current.onicecandidate = (e) => {
+            if (e.candidate) {
+                socket.current?.emit('webrtc signaling', { type: 'candidate', payload: e.candidate })
+            }
+        }
+
+        peer.current.ontrack = (e) => {
+            if (e && e.streams) {
+                if (themVideo.current) themVideo.current.srcObject = e.streams[0]
+                setStatus(3)
+            }
+        }
+
+        if (!remoteOffer) {
+            const offer = await peer.current?.createOffer()
+            peer.current.setLocalDescription(offer)
+            socket.current?.emit('webrtc signaling', { type: 'offer', payload: offer })
+        } else {
+            await peer.current.setRemoteDescription(remoteOffer)
+            const answer = await peer.current.createAnswer()
+            peer.current.setLocalDescription(answer)
+            socket.current?.emit('webrtc signaling', { type: 'answer', payload: answer })
+        }
+    }
+
+    const stopVideo = () => {
+        peer.current?.close()
+        stream.current?.getTracks().forEach((track) => track.stop())
+    }
+
     useEffect(() => {
+        if (!window.RTCPeerConnection || !navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) return () => Toast.fail('浏览器不支持，请切换浏览器')
+
         const urls = {
-            test: 'http://localhost:15002',
-            development: 'http://localhost:15002',
-            production: 'https://luoyisen.com:15002'
+            test: 'https://localhost:15002/OmgTV',
+            development: 'https://localhost:15002/OmgTV',
+            production: 'https://luoyisen.com:15002/OmgTV'
         }
-        const socket: Socket = io(urls[process.env.NODE_ENV])
+        socket.current = io(urls[process.env.NODE_ENV])
 
-        const onconnectHandler = () => {
-            console.log(socket.id) // x8WIv7-mJelg7on_ALbx
+        socket.current.on('connect', () => {
             Toast('连接服务器成功')
-        }
+        })
 
-        const ondisconnectHandler = () => {
-            console.log(socket.id) // undefined
+        socket.current.on('disconnect', () => {
             Toast('连接断开')
-        }
+        })
 
-        socket.on('connect', onconnectHandler)
-        socket.on('disconnect', ondisconnectHandler)
+        socket.current.on('userCount', setUserCount)
+
+        socket.current.on('match success', (id, start) => {
+            setStatus(2)
+            roomId.current = id
+            if (start) startSignaling(false)
+        })
+
+        socket.current.on('match waiting', () => {
+            setStatus(1)
+        })
+
+        socket.current.on('leaveRoom', () => {
+            setStatus(0)
+            stopVideo()
+        })
+
+        socket.current.on('webrtc signaling', (data) => {
+            switch (data.type) {
+                case 'offer':
+                    startSignaling(data.payload)
+                    break
+                case 'answer':
+                    peer.current?.setRemoteDescription(data.payload)
+                    break
+                case 'candidate':
+                    if (peer.current?.remoteDescription) peer.current?.addIceCandidate(data.payload)
+                    break
+                default:
+                    break
+            }
+        })
 
         return () => {
-            socket.disconnect()
-            socket.off('connect', onconnectHandler)
-            socket.off('disconnect', ondisconnectHandler)
+            socket.current?.disconnect()
+            stopVideo()
         }
-    })
+    }, [])
 
-    return <div>OmgTV开发中。。。，别催</div>
+    return (
+        <div className={`page ${styles.container}`}>
+            <div className={styles.videos}>
+                <video autoPlay ref={themVideo} className="them"></video>
+                <video autoPlay ref={meVideo} className={styles.me}></video>
+            </div>
+            <div className={styles.controls}>
+                <div className="shower">
+                    <p>在线人数：{userCount}</p>
+                </div>
+                <div className="btns">
+                    <Button type="primary" disabled={status != 0} loading={status === 1} onClick={startMatch}>
+                        开始匹配
+                    </Button>
+                    <Button type="danger" disabled={status !== 3} onClick={leaveRoom}>
+                        离开
+                    </Button>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default OmgTV
