e4402759a8994abd4d84b1bb713072d409f88aca
luoyisen <1402175410.@qq.com>
2024-04-22 15:02:58 +0800

    change route

--- src/resources/routes-constants.ts
+++ src/resources/routes-constants.ts
@@ -5,13 +5,13 @@ export type RouteObj = { path: string; name: string; authority: boolean; order: 
 export const ROUTES: { [key: string]: RouteObj } = {
     TEST_PAGE: { path: '/testPage', name: 'testPage', authority: isDebug, order: 0 },
     BROWSER_DETECT: { path: '/browserDetect', name: 'browserDetect', authority: isDebug, order: 0 },
+    BROWSER_FINGERPRINT: { path: '/browserFingerprint', name: 'browserFingerprint', authority: isDebug, order: 0 },
     HOMEPAGE_ROUTE: { path: '/', name: '首页', authority: false, order: 9999 },
     FACE_SYMMETRY: { path: '/faceSymmetry', name: '脸对称测试', authority: true, order: 9999 },
     AUTO_SOUND: { path: '/autoSound', name: '速读', authority: true, order: 9999 },
     FLOW_BORDER: { path: '/flowBorder', name: '流动边框', authority: true, order: 9999 },
     OMEG_TV: { path: '/omgTv', name: 'OmgTV', authority: true, order: 1 },
-    WECHAT_FEAT: { path: '/wechatFeat', name: 'wechatFeat', authority: true, order: 9999 },
-    BROWSER_FINGERPRINT: { path: '/browserFingerprint', name: 'browserFingerprint', authority: true, order: 9999 }
+    WECHAT_FEAT: { path: '/wechatFeat', name: 'wechatFeat', authority: true, order: 9999 }
 }
 
 export const TITLES: { [key: string]: string } = {}
