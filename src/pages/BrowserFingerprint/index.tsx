e9ca8484930f254c12e7a322b5a73ab31a0f6621
luoyisen <1402175410.@qq.com>
2024-04-22 14:57:51 +0800

    browser fingerprint

--- src/pages/BrowserFingerprint/index.tsx
+++ src/pages/BrowserFingerprint/index.tsx
@@ -1,5 +1,14 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import fingerprint from '@fingerprintjs/fingerprintjs'
+
 const BrowserFingerprint: React.FC = () => {
-    return <div>BrowserFingerprint deving...</div>
+    const [visitorId, setVisitorId] = useState<string>('')
+    useEffect(() => {
+        fingerprint
+            .load()
+            .then((_) => _.get())
+            .then((res) => setVisitorId(res.visitorId))
+    }, [])
+    return <div>{visitorId}</div>
 }
 export default BrowserFingerprint
