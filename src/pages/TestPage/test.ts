88d10d9e3c732ba50af2c021d4110b018626b693
luoyisen <1402175410.@qq.com>
2024-05-14 17:21:24 +0800

    fix

--- src/pages/TestPage/test.ts
+++ src/pages/TestPage/test.ts
@@ -13,21 +13,6 @@ function setDatas(obj: Partial<SrcObjTypes>) {
 setDatas({ age: 28, name: 'yxy' })
 console.log(srcObj)
 
-function watch<T>(obj: { [K in keyof T]: T[K] }): {
-    on: <K extends string & keyof T>(event: `${K}Changed`, callback: (oldValue: T[K], newValue: T[K]) => void) => void
-} {
-    return {
-        on(event, cb) {
-            const key = event.split('Changed')[0]
-            cb(obj[key], obj[key])
-        }
-    }
-}
-const watcher = watch(srcObj)
-watcher.on('nameChanged', (oldValue, newValue) => {
-    console.log(oldValue, newValue)
-})
-
 function setData<K extends keyof SrcObjTypes>(key: K, val: SrcObjTypes[K]) {
     srcObj[key] = val
 }
