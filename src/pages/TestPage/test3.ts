093dcd68313fcfa98fc644db0648df0d7e8baa0b
luoyisen <1402175410.@qq.com>
2024-07-03 09:15:37 +0800

    fix

--- src/pages/TestPage/test3.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-function sum(a: number, b: number) {
-    return a + b
-}
-declare function debounce<T extends Array<any>>(fn: (...arg: T) => any, delay: number): (...arg: T) => void
-
-const dSum = debounce(sum, 1000)
-const a = dSum(1, 2)
-a
