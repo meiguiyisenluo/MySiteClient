b223c08e9af8717ec93cdf370d858c7185d65170
luoyisen <1402175410.@qq.com>
2024-07-05 16:24:54 +0800

    fix

--- src/pages/Nitingdedao/index.tsx
+++ src/pages/Nitingdedao/index.tsx
@@ -1,33 +1,53 @@
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { PauseCircleO, PlayCircleO } from '@react-vant/icons'
 
 import styles from './index.module.scss'
 import lyric from './nitingdedao.lrc'
 
-console.log(
-    lyric.map((_) => {
-        const arr = _.time.split(':').map(Number).reverse()
-        let step = 1
-        const timestamp = arr.reduce((total, _) => {
-            const res = total + _ * step
-            step *= 60
-            return res
-        }, 0)
-        return {
-            ..._,
-            timestamp: timestamp - 3
-        }
-    })
-)
+// console.log(
+//     lyric.map((_) => {
+//         const arr = _.time.split(':').map(Number).reverse()
+//         let step = 1
+//         const timestamp = arr.reduce((total, _) => {
+//             const res = total + _ * step
+//             step *= 60
+//             return res
+//         }, 0)
+//         return {
+//             ..._,
+//             timestamp: timestamp - 3
+//         }
+//     })
+// )
 
 import RatioWBox from '@/components/RatioWBox.tsx'
 
 const srcObj = {
+    audioSrc: '/share/music/Jay/03.mp3',
     audioUrl: 'https://luoyisen.com/share/music/Jay/03.mp3',
     imgUrl: 'https://luoyisen.com/share/imgs/JayChou%40.1/6.png',
     name: '你听得到',
     author: '周杰伦',
     sign: 'J A Y'
+}
+
+// 音频分析
+let analyser: AnalyserNode | null = null
+let buffer: Uint8Array | null = null
+
+let animationId: number | undefined = undefined
+const update = (cb: (arr: Array<number>) => void) => {
+    if (animationId) cancelAnimationFrame(animationId)
+    animationId = requestAnimationFrame(() => update(cb))
+    if (!analyser) return
+    if (!buffer) return
+    analyser.getByteFrequencyData(buffer)
+    const offset = Math.floor((buffer.length * 2) / 3)
+    const datas = new Array(offset)
+    for (let i = 0; i < offset; i++) {
+        datas[i] = buffer[i]
+    }
+    cb(datas)
 }
 
 const Nitingdedao: React.FC = () => {
@@ -39,6 +59,8 @@ const Nitingdedao: React.FC = () => {
     const [duration, setDuration] = useState<number>(0)
 
     const audioRef = useRef<HTMLAudioElement>(null!)
+    const canvasRef = useRef<HTMLCanvasElement>(null!)
+    const ctx = useRef<CanvasRenderingContext2D>(null!)
 
     const togglePlaying = () => {
         if (playing) audioRef.current.pause()
@@ -50,22 +72,60 @@ const Nitingdedao: React.FC = () => {
         setDuration(audioRef.current.duration)
         if (audioRef.current.currentTime > lyric[lrcIdx + 1]?.timestamp ?? 9999) {
             setLrcIdx(lrcIdx + 1)
-        } else if (audioRef.current.currentTime <= lyric[0]?.timestamp ?? 0) {
+        }
+        // 切歌 或 单曲循环第二次开始播放
+        else if (audioRef.current.currentTime < 1) {
             setLrcIdx(0)
         }
     }
 
+    const draw = (arr: Array<number>) => {
+        if (!canvasRef.current) return
+        ctx.current.clearRect(0, -canvasRef.current.height / 2, canvasRef.current.width, canvasRef.current.height)
+        const rw = canvasRef.current.width / arr.length
+        for (let i = 0; i < arr.length; i++) {
+            const rh = arr[i] / 10
+
+            // 设置填充颜色
+            ctx.current.fillStyle = '#fff'
+            // 绘制矩形
+            ctx.current.fillRect(i * rw, 0, rw, rh)
+            ctx.current.fillRect(i * rw, -rh, rw, rh)
+        }
+    }
+
+    const onPlay = () => {
+        setPlaying(true)
+
+        if (analyser) return
+        if (!audioRef.current) return
+        const audioCtx = new AudioContext()
+        const source = audioCtx.createMediaElementSource(audioRef.current)
+        analyser = audioCtx.createAnalyser()
+        analyser.fftSize = 1024
+        buffer = new Uint8Array(analyser.frequencyBinCount)
+        source.connect(analyser)
+        analyser.connect(audioCtx.destination)
+        update(draw)
+    }
+
+    useEffect(() => {
+        canvasRef.current.width = 500
+        canvasRef.current.height = 61
+        ctx.current = canvasRef.current.getContext('2d')!
+
+        ctx.current.translate(0, canvasRef.current.height / 2)
+
+        return () => {
+            analyser = null
+            buffer = null
+            cancelAnimationFrame(animationId!)
+        }
+    }, [])
+
     return (
         <div className={`page ${styles.container}`} style={{ backgroundImage: `url(${srcObj.imgUrl})` }}>
-            <audio
-                ref={audioRef}
-                src={srcObj.audioUrl}
-                autoPlay
-                loop
-                onPlay={() => setPlaying(true)}
-                onPause={() => setPlaying(false)}
-                onTimeUpdate={onTimeUpdate}
-            ></audio>
+            <audio ref={audioRef} src={srcObj.audioSrc} autoPlay loop onPlay={onPlay} onPause={() => setPlaying(false)} onTimeUpdate={onTimeUpdate}></audio>
             <div className={styles.mask}></div>
             <RatioWBox wh_ratio={65}>
                 <div className={styles.content}>
@@ -87,23 +147,9 @@ const Nitingdedao: React.FC = () => {
                             <div className={styles.btns}>
                                 <div onClick={togglePlaying}>
                                     {!playing ? (
-                                        <PlayCircleO
-                                            onPointerEnterCapture={() => {
-                                                return
-                                            }}
-                                            onPointerLeaveCapture={() => {
-                                                return
-                                            }}
-                                        />
+                                        <PlayCircleO onPointerEnterCapture onPointerLeaveCapture />
                                     ) : (
-                                        <PauseCircleO
-                                            onPointerEnterCapture={() => {
-                                                return
-                                            }}
-                                            onPointerLeaveCapture={() => {
-                                                return
-                                            }}
-                                        />
+                                        <PauseCircleO onPointerEnterCapture onPointerLeaveCapture />
                                     )}
                                 </div>
                             </div>
@@ -115,7 +161,7 @@ const Nitingdedao: React.FC = () => {
                             <h2>{srcObj.name}</h2>
                             <h3>{srcObj.author}</h3>
                         </div>
-                        <canvas></canvas>
+                        <canvas ref={canvasRef}></canvas>
                         <div className={styles.word}>{lyric[lrcIdx].lyric}</div>
                     </div>
                 </div>
