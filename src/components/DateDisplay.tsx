250ba83148958145cf9b2db4b1d8a815707d4615
luoyisen <1402175410.@qq.com>
2024-08-07 15:46:57 +0800

    fix

--- src/components/DateDisplay.tsx
+++ src/components/DateDisplay.tsx
@@ -16,7 +16,7 @@ const DateDisplay: React.FC = () => {
 
     return (
         <div style={{ position: 'relative', width: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
-            <span style={{ color: 'orange', textAlign: 'center' }}>{`${date}  V0.3.1`}</span>
+            <span style={{ color: 'orange', textAlign: 'center' }}>{`${date}  V0.3.2`}</span>
         </div>
     )
 }
