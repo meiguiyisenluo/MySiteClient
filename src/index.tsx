824af695f3eb340d9bffd18b0f5a2838270db48a
luoyisen <1402175410@qq.com>
2025-04-18 08:44:32 +0800

    fix

--- src/index.tsx
+++ src/index.tsx
@@ -2,19 +2,19 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 
 import App from './App'
-import * as serviceWorker from './serviceWorker'
+// import * as serviceWorker from './serviceWorker'
 
 import { csrftoken } from '@/resources/api-constants'
 import { SESSION_STORAGE } from '@/resources/constants'
 
 import { isDebug } from '@/utility/env'
 
-import { report } from '@/resources/api-constants'
+// import { report } from '@/resources/api-constants'
 
 // eslint-disable-next-line @typescript-eslint/no-empty-function
 if (!isDebug) console.log = () => {}
 
-report({ event: 'total_pv' })
+// report({ event: 'total_pv' })
 
 const container = document.getElementById('root')
 if (container) {
@@ -36,7 +36,7 @@ if (container) {
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register()
+// serviceWorker.register()
 
 // document.addEventListener('keydown', function (event) {
 //     if ((event.ctrlKey || event.metaKey) && (event.key === '+' || event.key === '-' || event.key === '0')) {
@@ -54,15 +54,15 @@ serviceWorker.register()
 // )
 
 window._githubSrc = 'https://github.com/meiguiyisenluo/MySiteClient'
-if (window.Notification) {
-    Notification.requestPermission().then(function (permission) {
-        if (permission === 'granted') {
-            const notification = new Notification('Hi!', {
-                body: 'This code is open source in ' + window._githubSrc
-            })
-            notification.addEventListener('click', () => {
-                window.open(window._githubSrc)
-            })
-        }
-    })
-}
+// if (window.Notification) {
+//     Notification.requestPermission().then(function (permission) {
+//         if (permission === 'granted') {
+//             const notification = new Notification('Hi!', {
+//                 body: 'This code is open source in ' + window._githubSrc
+//             })
+//             notification.addEventListener('click', () => {
+//                 window.open(window._githubSrc)
+//             })
+//         }
+//     })
+// }
